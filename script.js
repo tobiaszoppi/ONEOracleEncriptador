7f4e71c11da35bf0b50d7a88a07b1a504b0dc7fd
Tobi <tobias_zoppi@hotmail.com>
2023-04-15 06:56:41 -0300

    estilando codigo

--- script.js
+++ script.js
@@ -8,28 +8,28 @@ const resultado = document.getElementById("resultado");
 
 // Función para mostrar el mensaje de "No encontrado"
 function mostrarNotFound() {
-    resultado.style.display= "none";
+    resultado.style.display = "none";
     notFound.style.display = "block";
 }
 
 // Función para encriptar la cadena de caracteres
 function encriptar(cadena) {
-  cadena = cadena.replace(/e/g, "enter");
-  cadena = cadena.replace(/i/g, "imes");
-  cadena = cadena.replace(/a/g, "ai");
-  cadena = cadena.replace(/o/g, "ober");
-  cadena = cadena.replace(/u/g, "ufat");
-  return cadena;
+    cadena = cadena.replace(/e/g, "enter");
+    cadena = cadena.replace(/i/g, "imes");
+    cadena = cadena.replace(/a/g, "ai");
+    cadena = cadena.replace(/o/g, "ober");
+    cadena = cadena.replace(/u/g, "ufat");
+    return cadena;
 }
 
 // Función para desencriptar la cadena de caracteres
 function desencriptar(cadena) {
-  cadena = cadena.replace(/enter/g, "e");
-  cadena = cadena.replace(/imes/g, "i");
-  cadena = cadena.replace(/ai/g, "a");
-  cadena = cadena.replace(/ober/g, "o");
-  cadena = cadena.replace(/ufat/g, "u");
-  return cadena;
+    cadena = cadena.replace(/enter/g, "e");
+    cadena = cadena.replace(/imes/g, "i");
+    cadena = cadena.replace(/ai/g, "a");
+    cadena = cadena.replace(/ober/g, "o");
+    cadena = cadena.replace(/ufat/g, "u");
+    return cadena;
 }
 
 function copiarTexto() {
@@ -37,40 +37,40 @@ function copiarTexto() {
     texto.select();
     document.execCommand("copy");
     alert("Texto copiado al portapapeles");
-  }  
+}
 
 // Evento de clic en el botón "Encriptar"
 btnEncriptar.addEventListener("click", () => {
-  const texto = textoEntrada.value;
-  if (texto.trim() !== "") {
-    const textoEncriptado = encriptar(texto);
+    const texto = textoEntrada.value;
+    if (texto.trim() !== "") {
+        const textoEncriptado = encriptar(texto);
 
-    const textoSalida = document.getElementById("texto-salida");
-    const btnCopiar = document.getElementById("btn-copiar");
-    
-    textoSalida.value = textoEncriptado;
-    notFound.style.display = "none";
-    btnCopiar.addEventListener("click", copiarTexto);
-    resultado.style.display = "flex";
-  } else {
-    mostrarNotFound();
-  }
+        const textoSalida = document.getElementById("texto-salida");
+        const btnCopiar = document.getElementById("btn-copiar");
+
+        textoSalida.value = textoEncriptado;
+        notFound.style.display = "none";
+        btnCopiar.addEventListener("click", copiarTexto);
+        resultado.style.display = "flex";
+    } else {
+        mostrarNotFound();
+    }
 });
 
 // Evento de clic en el botón "Desencriptar"
 btnDesencriptar.addEventListener("click", () => {
-  const texto = textoEntrada.value;
-  if (texto.trim() !== "") {
-    const textoDesencriptado = desencriptar(texto);
-    
-    const textoSalida = document.getElementById("texto-salida");
-    const btnCopiar = document.getElementById("btn-copiar");
+    const texto = textoEntrada.value;
+    if (texto.trim() !== "") {
+        const textoDesencriptado = desencriptar(texto);
 
-    textoSalida.value = textoDesencriptado;
-    notFound.style.display = "none";
-    btnCopiar.addEventListener("click", copiarTexto);
-    resultado.style.display = "flex";
-  } else {
-    mostrarNotFound();
-  }
+        const textoSalida = document.getElementById("texto-salida");
+        const btnCopiar = document.getElementById("btn-copiar");
+
+        textoSalida.value = textoDesencriptado;
+        notFound.style.display = "none";
+        btnCopiar.addEventListener("click", copiarTexto);
+        resultado.style.display = "flex";
+    } else {
+        mostrarNotFound();
+    }
 });
